4dfd8e5fcb75be7d4aa9c2171b709ed62420cbd7
LebedevIlyaG <lebedev.ilya.g@gmail.com>
2025-08-08 11:46:57 +0300

    Поправлена работа с задачами

--- gen/vs-17_64-no_mp-No_MPI.bat
+++ gen/vs-17_64-no_mp-No_MPI.bat
@@ -7,7 +7,7 @@ git submodule update
 cd %ROOT_DIR%
 if not exist build_64 mkdir build_64
 cd build_64
-cmake -G "Visual Studio 17 2022" -DGLOBALIZER_BENCHMARKS_USE_CUDA=OFF -DGLOBALIZER_BENCHMARKS_BUILD_TESTS=ON -Drastrigin_build=ON -DX2_build=ON ..
+cmake -G "Visual Studio 17 2022" -DGLOBALIZER_BENCHMARKS_BUILD_APPLICATION=ON -DGLOBALIZER_BENCHMARKS_USE_CUDA=OFF -DGLOBALIZER_BENCHMARKS_BUILD_TESTS=ON -Drastrigin_build=ON -DX2_build=ON ..
 
 Globalizer_Benchmarks.sln
 
